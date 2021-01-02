a975a7d67857c54a37ae9c8f9ddc472933871f54
dleo9307 <dleo9307@naver.com>
2021-01-02 19:11:13 +0900

    url 변경

--- src/module/muscle/muscle.controller.ts
+++ src/module/muscle/muscle.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { MuscleService } from './muscle.service';
 import { MuscleCreateInput } from './dto/muscle.dto';
 
-@Controller('muscles')
+@Controller('muscle ')
 export class MuscleController {
   constructor(private readonly muscleService: MuscleService) {}
 
